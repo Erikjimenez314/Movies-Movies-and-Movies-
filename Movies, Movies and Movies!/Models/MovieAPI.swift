f603a7aa50861674ceb35b115e5039c62aa4dbfa
Erik Jimenez <Erikjimenez314@gmail.com>
2022-06-07 01:36:32 -0400

    Other

---
+++ Movies, Movies and Movies!/Models/MovieAPI.swift
@@ -0,0 +1,55 @@
+//
+//  MovieAPI.swift
+//  Movies, Movies and Movies!
+//
+//  Created by Erik Jimenez on 6/6/22.
+//
+
+import Foundation
+
+// MARK: - MovieTopLevelObject
+struct MovieTopLevelObject: Decodable {
+    let page: Int
+    let results: [Movie]
+    
+}
+
+// MARK: - Movie
+struct Movie: Decodable{
+    let originalTitle: String
+    let posterPath: URL?
+    let overview: String
+    let rating: Double
+    let id: Int
+    
+    enum CodingKeys: String, CodingKey{
+        case originalTitle = "original_title"
+        case posterPath = "poster_path"
+        case overview = "overview"
+        case rating = "vote_average"
+        case id = "id"
+    }
+}
+
+// MARK: - CastTopLevelObject
+struct CastTopLevelObject: Decodable {
+    
+    let cast: [Cast]
+}
+
+// MARK: - Cast
+struct Cast: Decodable{
+    let id: Int
+    let name: String
+    let knownForDepartment: String
+    let character: String
+    let profilePath: URL?
+    
+    enum CodingKeys: String, CodingKey{
+        case id
+        case name, character
+        case knownForDepartment = "known_for_department"
+        case profilePath = "profile_path"
+    }
+}
+
