268a16013b60ba8e2c44e4572059eddc9dc82a98
Erik Jimenez <Erikjimenez314@gmail.com>
2022-06-07 01:36:17 -0400

    Other

---
+++ Movies, Movies and Movies!/Model Controllers/StudioGhibliAPIController.swift
@@ -0,0 +1,47 @@
+//
+//  StudioGhibliAPIController.swift
+//  Movies, Movies and Movies!
+//
+//  Created by Erik Jimenez on 6/6/22.
+//
+
+import Foundation
+
+class StudioGhibliAPIController{
+    
+    // MARK: - Properties
+    static let shared = StudioGhibliAPIController()
+    
+    // Studio Ghibli API
+    static let baseURL = URL(string: "https://ghibliapi.herokuapp.com")
+    static let filmComponent = "films"
+    
+} // End of class
+
+//Main fetch functions
+extension StudioGhibliAPIController{
+    
+    static func fetchFilms(completion: @escaping(Result<[Film], NetworkError>) -> Void){
+        
+        guard let baseURL = baseURL else { return completion(.failure(.invalidURL))}
+        let finalURL = baseURL.appendingPathComponent(filmComponent)
+        
+        print(finalURL)
+        let task = URLSession.shared.dataTask(with: finalURL) { data, _, error in
+            if let error = error{
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion(.failure(.thrownError(error)))
+            }
+            
+            guard let data = data else { return completion(.failure(.noData))}
+            
+            do{
+                let films = try JSONDecoder().decode([Film].self, from: data)
+                completion(.success(films))
+            }catch{
+                completion(.failure(.unableToDecode))
+            }
+        }
+        task.resume()
+    }
+} // End of Extension
